a86c331f7539f3837533374cfb8c04ce0523fcb3
Med12-q <smashsouare82@gmail.com>
2026-05-02 14:30:06 +0000

    ✅ Fix token.js - syntax errors fixed, improved art

---
+++ token.js
@@ -0,0 +1,6 @@
+require('dotenv').config();
+
+module.exports = {
+  BOT_TOKEN: process.env.BOT_TOKEN || '8734607446:AAHpxk0hliYdvDBhyA-qS8VvjhwOdRfJGpM',
+  startupPassword: process.env.STARTUP_PASSWORD || 'Optimusxmdv2'
+};
