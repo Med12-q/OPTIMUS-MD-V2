d4c2ebaa2c55139586fc6f97c97cbc84f7463cca
Med12-q <smashsouare82@gmail.com>
2026-05-16 10:29:12 +0000

    🔧 Fix ESM crash + new commands: token.js

--- token.js
+++ token.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-  BOT_TOKEN: process.env.BOT_TOKEN || '8734607446:AAHpxk0hliYdvDBhyA-qS8VvjhwOdRfJGpM',
+  BOT_TOKEN: process.env.BOT_TOKEN || '8966598759:AAESiRD5yScXvbG4kKkmDc2U7x_6ryDVSSQ',
   startupPassword: process.env.STARTUP_PASSWORD || 'Optimusxmdv2'
 };
