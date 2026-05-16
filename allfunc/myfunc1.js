704e22ea77389f23dd8ed0b803af1457e8f97d48
Med12-q <smashsouare82@gmail.com>
2026-05-16 11:13:23 +0000

    🔧 Fix ESM crash via baileys-shim: allfunc/myfunc1.js

--- allfunc/myfunc1.js
+++ allfunc/myfunc1.js
@@ -7,7 +7,7 @@ const {
     proto,
     delay,
     getContentType
-} = require('@whiskeysockets/baileys')
+} = require('./baileys-shim')
 const chalk = require('chalk')
 const fs = require('fs')
 const Crypto = require('crypto')
